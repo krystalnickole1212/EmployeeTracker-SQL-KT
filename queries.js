99121c435b137b69496ccb9c0ed17b6d05ad2ecb
krystalnickole1212 <krystalnickole_12@yahoo.com>
2022-04-30 21:17:04 -0500

    added more from office hours

--- queries.js
+++ queries.js
@@ -2,9 +2,20 @@ const db = require ('./config.connection.js');
 
 const viewDepartment = async function(){
     //open employeeDB
-    return await db.query(
+    //promise wrapper
+    return new Promise(function (resolve, reject) {
+        db.query(
         'SELECT * FROM `employeeDB.department`;' 
-    )
-};
+        , (error(res));
+        )};
+        
+const viewRoles = async function(){
+//open employeeDB
+//promise wrapper
+return new Promise(function (resolve, reject) {
+    db.query(
+    'SELECT * FROM `employeeDB.role`;' 
+    , (error(res));
+    )};
 
-module.exports={viewDepartment}
+module.exports={viewDepartment, viewRoles}
